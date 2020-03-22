71f728500d072ddf8d556151ea5e95e36b5de184
mirynth <michellewong.nma@gmail.com>
2020-03-21 19:30:26 -0700

    added test script

---
+++ compile.sh
@@ -0,0 +1,3 @@
+export CLASSPATH=".:$HOME/Downloads/selenium-server-standalone-3.141.59.jar:$HOME/Downloads/testng-6.8.7.jar"
+javac $HOME/Documents/GitHub/LAMP-Selenium-TestLibrary/TecAdminSeleniumTest.java
+java TecAdminSeleniumTest
