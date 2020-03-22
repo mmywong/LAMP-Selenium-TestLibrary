7e6fb591e4dc732dca1ab482456d2c548d9c1867
mirynth <michellewong.nma@gmail.com>
2020-03-21 23:56:45 -0700

    Added test to check login function

--- selenium_test.java
+++ selenium_test.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
- 
-public class TecAdminSeleniumTest {
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebElement;
+
+public class selenium_test {
  
         public static void main(String[] args) throws IOException, InterruptedException {
                 System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
@@ -18,8 +20,14 @@ public class TecAdminSeleniumTest {
                 driver.get("http://192.168.1.240/simple-lamp/index.php");
  
                 Thread.sleep(1000);
- 
-                if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+		
+		WebElement searchbar = driver.findElement(By.id("username"));
+		searchbar.sendKeys("michelle");
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
+		
+		System.out.println("testing searchbar");
+
+                if (driver.getPageSource().contains("Logout")) {
                         System.out.println("Pass");
                 } else {
                         System.out.println("Fail");
